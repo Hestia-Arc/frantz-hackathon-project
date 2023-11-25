cd6687ce2271f2b40205444af6b08b04f1458766
Afolabi Esther <starafolab@gmail.com>
2023-11-25 18:48:28 +0100

    adjust layouts -add transition and states

--- app.js
+++ app.js
@@ -2,30 +2,36 @@
 class SVG extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="27"
-          height="27"
-          viewBox="0 0 32 32"
-          fill="none"
-          style="padding-top: 5px"
-        >
-          <circle
-            cx="16"
-            cy="16"
-            r="12"
-            stroke="#8a8a8a"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="4 6"
-          />
-        </svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <circle cx="12" cy="12" r="10" stroke="#8A8A8A" stroke-width="2.08333" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="5 5"/>
+</svg>
+       
 
         `;
   }
 }
+
+ 
+        // <svg
+        //   xmlns="http://www.w3.org/2000/svg"
+        //   width="27"
+        //   height="27"
+        //   viewBox="0 0 32 32"
+        //   fill="none"
+        //   style="padding-top: 5px"
+        // >
+        //   <circle
+        //     cx="16"
+        //     cy="16"
+        //     r="12"
+        //     stroke="#8a8a8a"
+        //     stroke-width="2.5"
+        //     stroke-linecap="round"
+        //     stroke-linejoin="round"
+        //     stroke-dasharray="4 6"
+        //   />
+        // </svg>
 
 // ===================FEATURES
 //##### 1. click logo and navigate to landing page
@@ -49,8 +55,10 @@ const notifyBox = document.querySelector(".notify-dropdown");
 const profileBox = document.querySelector(".profile");
 const menuBox = document.querySelector(".profile-menu");
 const stepDown = document.querySelector(".step-down");
-const stepPanels = document.querySelector(".checks-box");
-
+const accordionPanels = document.querySelector(".accordion-accordion");
+const checkTags = document.querySelectorAll(".check-tag");
+const checkInfos = document.querySelector(".check-infos");
+const checkImg = document.querySelector(".check-img");
 
 // ========================3...NOTIFICATION BELL TOGGLE
 notifyIcon.addEventListener("click", () => {
@@ -64,9 +72,23 @@ profileBox.addEventListener("click", () => {
 
 // ======================6...TRIAL BOX REMOVE
 
-
-
 // ======================7...EXPAND STEPS
 stepDown.addEventListener("click", () => {
-  stepPanels.classList.toggle("hidden");
+  accordionPanels.classList.toggle("hidden");
+});
+
+// ======================8...EXPAND PANELS
+checkTags.forEach((tag) => {
+  tag.addEventListener("click", () => {
+    tag.nextElementSibling.classList.toggle("hidden");
+    tag.parentNode.parentNode.classList.toggle("check-bg");
+
+    // prev
+    tag.parentNode.parentNode.previousElementSibling.classList.remove(
+      "check-bg"
+    );
+    tag.parentNode.parentNode.previousElementSibling.children[1].firstElementChild.nextElementSibling.classList.add(
+      "hidden"
+    );
+  });
 });
